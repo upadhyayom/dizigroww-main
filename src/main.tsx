fc0861726c7df40b967c09277c10807405abe782
qwert <qwert@qwerts-MacBook-Air.local>
2026-04-10 00:29:40 +0530

    fix: inject missing SEO dependency and mount Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
